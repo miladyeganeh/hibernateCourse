0d4b6f836bdda040b018bdf4fb44f7e9983ef1a9
milad <milad.yeganeh@gmail.com>
2019-02-07 22:47:01 +0330

    main

--- src/main/java/com/dotin/course/Main.java
+++ src/main/java/com/dotin/course/Main.java
@@ -1,4 +1,25 @@
 package com.dotin.course;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldpu");
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        Message message = new Message();
+        message.setMessage("hello");
+
+        entityManager.persist(message);
+
+        entityManager.getTransaction().commit();
+
+    }
 }
---
+++ src/main/java/com/dotin/course/Message.java
@@ -0,0 +1,31 @@
+package com.dotin.course;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
