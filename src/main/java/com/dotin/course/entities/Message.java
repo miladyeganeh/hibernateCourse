2894e6d057f5e289ffc29357969354e1b0c8ddab
milad <milad.yeganeh@gmail.com>
2019-02-09 12:22:13 +0330

    main

--- src/main/java/com/dotin/course/entities/Message.java
+++ src/main/java/com/dotin/course/entities/Message.java
@@ -1,5 +1,6 @@
 package com.dotin.course.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ public class Message {
     @GeneratedValue
     private Long id;
 
+    @Column
     private String message;
 
     public Long getId() {
